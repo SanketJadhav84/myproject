43f4dc0f2c0e240e52228ab80677c9dd53e91666
Sanket Jadhav <sanket25041@gmail.com>
2026-01-09 12:58:54 +0530

    Initial commit - Flask EC2 dashboard project

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
 class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'sanket@0000'
-    MYSQL_DB = 'aws_project'
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_USER = os.getenv("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
+    MYSQL_DB = os.getenv("MYSQL_DB", "aws_project")
     MYSQL_CURSORCLASS = 'DictCursor'
-    SECRET_KEY = '123456789'
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
